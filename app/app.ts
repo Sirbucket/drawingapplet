dfd514b5242d072b3a4ae260365a8b6896eaa5e9
William Tuleja <51178809+Sirbucket@users.noreply.github.com>
2022-02-27 04:27:19 +0000

    Finished for now

--- app/app.ts
+++ app/app.ts
@@ -1,55 +1,8 @@
 //App
 import {ctx, canvas} from "./canvas";
-import {makeButton} from "./controls";
-import {
-		caps,
-		connections,
-		colors,
-		eraserButton,
-		clearButton,
-		drawInLinesButton,
-		drawInFillsButton,
-		saveButton,
-} from "./appDefinitions";
-import {each} from "lodash"; 
+import {drawInFillsButton,drawInLinesButton,eraserButton} from "./appDefinitions"
 
 let fm = false;
-
-//Capping method.
-each(caps, (cap : string) => {
-	const capButton : HTMLButtonElement = makeButton("Cap " + cap);
-	capButton.addEventListener("click", () => {
-		//Fake error, typescript is dumb.
-		ctx.lineCap = cap;
-	});
-});
-//How the line connects visually.
-each(connections, (connection : string) => {
-	const connectionButton : HTMLButtonElement = makeButton("Connect " + connection);
-	connectionButton.addEventListener("click", () => {
-		ctx.lineJoin = connection;
-	});
-});
-//Colors
-each(colors,color => {
-	const colorButton : HTMLButtonElement = makeButton(color);
-	colorButton.style.background = color;
-	colorButton.addEventListener("click", () => {
-		ctx.fillStyle = color;
-		ctx.strokeStyle = color;
-	})
-})
-
-//Clears canvas
-clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-eraserButton.addEventListener("click", () => {
-  fm = true;
-  ctx.strokeStyle = "white";
-  ctx.fillStyle = "white";
-});
 
 //Checks to draw in fills or not.
 drawInFillsButton.addEventListener("click", () => {
@@ -62,13 +15,13 @@ drawInLinesButton.addEventListener("click", () => {
   ctx.strokeStyle = "black";
 });
 
-//Save button
-saveButton.addEventListener("click", () => {
-  const link = document.getElementById("link");
-  link.setAttribute("download", "CanvasImage.png");
-  link.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-  link.click();
+//Eraser Button
+eraserButton.addEventListener("click", () => {
+  fm = true;
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
 });
+
 
 //Drawing functionality, first listener checks mouse down then offsets the initial moveto. Mousemove draws a line where your mouse is moving while mouse down, then determins whether to stroke or fill based on fillmode being true or false.
 canvas.addEventListener("mousedown",event => {
--- app/appDefinitions.ts
+++ app/appDefinitions.ts
@@ -1,29 +1,30 @@
 //Definitions for app
 import {makeButton} from "./controls";
 import {ctx,canvas} from "./canvas"
+import {each} from "lodash"
 
 const lw = canvas.width/100
+
 //Dynamic lineWidth, written cheaply.
 window.addEventListener("load", () => {
 	ctx.lineWidth = lw
-	
 });
 window.addEventListener("resize", () => {
 	ctx.lineWidth = lw
 });
 
-export const caps = [
+const caps = [
 	"round",
 	"square",
 ];
 
-export const connections = [
+const connections = [
 	"bevel",
 	"round",
 	"miter",
 ];
 
-export const colors = [
+const colors = [
 	"Red",
 	"Blue",
 	"Green",
@@ -43,3 +44,43 @@ export const clearButton = makeButton(" 🗑 ");
 export const drawInLinesButton = makeButton("Draw in Lines");
 export const drawInFillsButton = makeButton("Draw In Fills");
 export const saveButton = makeButton("Save");
+
+//Capping method.
+each(caps, (cap : string) => {
+	const capButton : HTMLButtonElement = makeButton("Cap " + cap);
+	capButton.addEventListener("click", () => {
+		//Fake error, typescript is dumb.
+		ctx.lineCap = cap;
+	});
+});
+
+//How the line connects visually.
+each(connections, (connection : string) => {
+	const connectionButton : HTMLButtonElement = makeButton("Connect " + connection);
+	connectionButton.addEventListener("click", () => {
+		ctx.lineJoin = connection;
+	});
+});
+
+//Colors
+each(colors,color => {
+	const colorButton : HTMLButtonElement = makeButton(color);
+	colorButton.style.background = color;
+	colorButton.addEventListener("click", () => {
+		ctx.fillStyle = color;
+		ctx.strokeStyle = color;
+	})
+})
+
+//Clears canvas
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+//Save button
+saveButton.addEventListener("click", () => {
+  const link = document.getElementById("link");
+  link.setAttribute("download", "CanvasImage.png");
+  link.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+});
